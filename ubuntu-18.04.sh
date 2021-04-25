24c57c20b9ebe6456d5185f74f05087556411ae9
Alex Naidis <alex.naidis@linux.com>
2021-04-25 02:29:20 +0200

    ubuntu-script: Various improvements for the install script
    - Add dist-upgrade command during ubuntu updating
    - Use https for postgresql source repo
    - Fix one of the install commands being interactive (missing -y)
    - Remove redundant commands
    - Minor style changes

--- ubuntu-18.04.sh
+++ ubuntu-18.04.sh
@@ -2,22 +2,20 @@
 
 start=`date +%s`
 
-echo "updaing ubuntu..."
+echo "updating ubuntu ..."
 sudo apt-get -y update
 sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
 
 # python 3.8
 echo "installing Python 3.8 ..."
 sudo apt-get -y install gcc binutils
-sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update -y
 sudo apt-get -y install python-pip
 sudo apt-get -y install python-setuptools
 sudo apt-get -y install build-essential python3.8-dev
-sudo apt-get update
-sudo apt-get install build-essential
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 9
 sudo update-alternatives  --set python /usr/bin/python3.8
@@ -26,7 +24,6 @@ sudo apt-get -y install python-dev
 sudo apt-get -y install python3-pip
 python -m pip install --upgrade pip
 hash -d pip
-pip install -U setuptools
 pip install --upgrade setuptools
 pip install ez_setup
 pip install Cython numpy
@@ -34,10 +31,10 @@ pip install Cython numpy
 
 # talib
 echo "installing talib ..."
-sudo apt-get -y install libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev python3.8-dev
+sudo apt-get -y install libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz -q
 tar -xzf ta-lib-0.4.0-src.tar.gz
-cd ta-lib/ 
+cd ta-lib/
 ./configure --prefix=/usr
 make
 sudo make install
@@ -46,15 +43,16 @@ sudo make install
 echo "installing PostgreSQL ..."
 cd
 sudo apt-get install wget ca-certificates
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update -y
 sudo apt-get install -y postgresql postgresql-contrib python-psycopg2 libpq-dev
 
 # install screen
-echo "installing Screen..."
+echo "installing Screen ..."
 sudo apt-get install -y screen
 
-# install pip packages
+# install pip packages for jesse
+echo "installing jesse ..."
 pip install -r https://raw.githubusercontent.com/jesse-ai/jesse/master/requirements.txt
 pip install jesse
 
